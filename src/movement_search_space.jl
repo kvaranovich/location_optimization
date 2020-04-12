8ebfe2446f403bd1101fd21315d6496018de6fb1
Kiryl <k.varanovich@gmail.com>
2020-04-12 13:15:08 +0200

    Added enumeration to loop

--- src/movement_search_space.jl
+++ src/movement_search_space.jl
@@ -17,9 +17,9 @@ end
 function find_possible_movements_radius(mx::MapData, positions::Vector{Int}, y, r::Float64 = 150.0)
     ORIGIN_DESTINATION = []
 
-    for amb in positions
-        nodes, times = y(mx, [amb], r)
-        origin_destination = collect(Iterators.product(amb, nodes))
+    for (index, value) in enumerate(positions)
+        nodes, times = y(mx, [value], r)
+        origin_destination = collect(Iterators.product(value, nodes, index))
         append!(ORIGIN_DESTINATION, origin_destination)
     end
 
