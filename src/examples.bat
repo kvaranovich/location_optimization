8947e4126f20a144ef7f7601f102d0485a1da9c1
Kiryl <k.varanovich@gmail.com>
2020-06-20 12:15:10 +0200

    Updated examples

--- src/examples.bat
+++ src/examples.bat
@@ -1,3 +1,4 @@
 julia p_mp_model_jump.jl --city winnipeg --metric distance --p 9 --m 20 --n 200 --q 1
 julia mclp_model_jump.jl --city winnipeg --metric distance --p 9 --m 20 --n 200 --q 1 --r 3000.00
-julia iterative_model.jl --city winnipeg --metric time --p 9 --r 100.00 --ruin_random 0.5
+julia iterative_model.jl --city winnipeg --metric time --p 9 --r 100.00 --R 600.0 --q 1 --ruin_random 0.5
+julia hierarchical_iterative_model.jl --city winnipeg --metric time --p 9 --r 400.00 --R 600.0 --q 1 --ruin_random 0.5
